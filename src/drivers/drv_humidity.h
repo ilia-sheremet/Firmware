9811196e31c5f654de3b9db9efa98fb9be79d368
ilia-sheremet <illia.sheremet@gmail.com>
2015-08-12 18:12:14 +0100

    Humidity works, but not pressure. No apm backend level
    Temp and humidity work. No backend for APM
    
    Cycle implemented, no ioctl and read yet
    
    Drivers work. Need correction on code and improved redunduncy

---
+++ src/drivers/drv_humidity.h
@@ -0,0 +1,41 @@
+/*
+ * drv_humidity.h
+ *
+ *  Created on: Jul 9, 2015
+ *      Author: postal
+ */
+
+#ifndef DRV_HUMIDITY_H_
+#define DRV_HUMIDITY_H_
+
+#include <stdint.h>
+#include <sys/ioctl.h>
+
+#include "drv_sensor.h"
+#include "drv_orb_dev.h"
+
+#define HTU21D_DEVICE_PATH	"/dev/htu21d"
+
+#define HTU21D_CONVERSION_INTERVAL 	100000 /* 60ms for one sonar */
+#define TICKS_BETWEEN_SUCCESIVE_FIRES 	100000 /* 30ms between each sonar measurement (watch out for interference!) */
+                                        // maybe try (1000000 / 10)	, 10 hz
+
+/* oddly, ERROR is not defined for c++ */
+#ifdef ERROR
+# undef ERROR
+#endif
+static const int ERROR = -1;
+
+#ifndef CONFIG_SCHED_WORKQUEUE
+# error This requires CONFIG_SCHED_WORKQUEUE.
+#endif
+
+
+struct hum_report {
+	uint64_t timestamp;
+	uint64_t error_count;
+	float				_humidity;
+};
+
+
+#endif /* DRV_HUMIDITY_H_ */
